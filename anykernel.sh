3e27ce03c50b178c79135c359440904645821fbc
Feinzer <danhernan23i@gmail.com>
2019-05-19 06:54:16 -0400

    Fix vendor installation

--- anykernel.sh
+++ anykernel.sh
@@ -35,19 +35,17 @@ dump_boot;
 
 # init.rc
 ui_print " ";
-mount -o rw -t auto /system;
-if [ -f /system/vendor/etc/init/hw/init.bacon.rc ]; then
-  ui_print " Adding Fz init patch in /system/vendor";
-  cp /tmp/anykernel/ramdisk/init.fz.rc /system/vendor/etc/init/hw/init.fz.rc;
-  insert_line /system/vendor/etc/init/hw/init.bacon.rc "init.fz" after "import /vendor/etc/init/hw/init.qcom-common.rc" "import /vendor/etc/init/hw/init.fz.rc";
-  chmod 644 /system/vendor/etc/init/hw/init.fz.rc;
+mount -o rw,remount -t auto /system;
+if [ -d /system ] && [ -f /system/vendor/etc/init/hw/init.qcom.power.rc ]; then
+  ui_print "- Applying Fz init patch in /system/vendor"
+  insert_line /system/vendor/etc/init/hw/init.bacon.rc "init.fz.rc" after "import /vendor/etc/init/hw/init.qcom-common.rc" "import /init.fz.rc";
+  chmod 644 /init.fz.rc;
 else
-  ui_print " Adding Fz init patch in /";
-  cp /tmp/anykernel/ramdisk/init.fz.rc /init.fz.rc;
-  insert_line init.bacon.rc "init.fz.rc" after "import /init.qcom.power.rc" "import /init.fz.rc";
+  ui_print "- Applying Fz init patch in /"
+  insert_line init.bacon.rc "init.fz" before "import /init.usb.rc" "import /init.fz.rc";
   chmod 644 /init.fz.rc;
 fi
-umount /system;
+mount -o ro,remount -t auto /system;
 # end ramdisk changes
 
 write_boot;
