c6f12241ff4e0b10792afa29fcf23e70b2a1aa48
Feinzer <danhernan23i@gmail.com>
2019-05-19 06:46:19 -0400

    yeet

--- anykernel.sh
+++ anykernel.sh
@@ -34,14 +34,15 @@ dump_boot;
 # begin ramdisk changes
 
 # init.rc
+ui_print " ";
 mount -o rw -t auto /system;
 if [ -f /system/vendor/etc/init/hw/init.bacon.rc ]; then
-  ui_print "       Adding Fz init patch in /system/vendor";
+  ui_print " Adding Fz init patch in /system/vendor";
   cp /tmp/anykernel/ramdisk/init.fz.rc /system/vendor/etc/init/hw/init.fz.rc;
   insert_line /system/vendor/etc/init/hw/init.bacon.rc "init.fz" after "import /vendor/etc/init/hw/init.qcom-common.rc" "import /vendor/etc/init/hw/init.fz.rc";
   chmod 644 /system/vendor/etc/init/hw/init.fz.rc;
 else
-  ui_print "       Adding Fz init patch in /";
+  ui_print " Adding Fz init patch in /";
   cp /tmp/anykernel/ramdisk/init.fz.rc /init.fz.rc;
   insert_line init.bacon.rc "init.fz.rc" after "import /init.qcom.power.rc" "import /init.fz.rc";
   chmod 644 /init.fz.rc;
